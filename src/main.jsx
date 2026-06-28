763818002b009252c2f381fc897222f0fd77ef97
Ethan Joseph <ethancredo.ascc@outlook.com>
2026-06-29 00:28:28 +0800

    initialize files

---
+++ src/main.jsx
@@ -0,0 +1,40 @@
+// import { createRoot } from 'react-dom/client';
+
+// const { createElement } = require("react");
+
+// const root = createRoot(document.getElementById('root'));
+
+// function MyNavbar() {
+// 	return (
+// 		<nav>
+// 			<h1>Logo</h1>
+// 			<ul>
+// 				<li>Home</li>
+// 				<li>About</li>
+// 				<li>Contact</li>
+// 			</ul>
+// 		</nav>
+// 	)
+// }
+
+// function MainContent() {
+// 	return (
+// 		<h1>React is great!</h1>
+// 	)
+// }
+
+// root.render(
+// 	<>
+// 		<MyNavbar />
+// 		<MainContent />	
+// 	</>
+// )
+
+
+const h1 = document.createElement('h1');
+const root = document.getElementById('root');
+
+h1.textContent = 'Hello!'
+h1.classList.add('header');
+
+root.append(h1);
